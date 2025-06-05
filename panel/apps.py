294f461642dbe73a258d672cb4a2533c283c92ea
verhovv <vladimirverhov@yandex.ru>
2025-06-06 01:50:38 +1000

    signals

--- panel/apps.py
+++ panel/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PanelConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'panel'
+
+    def ready(self):
+        import panel.signals
